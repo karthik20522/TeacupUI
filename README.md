5db6c513c8ab66aff9517e029abd162358bb12da
Karthik Srinivasan <karthik20522@yahoo.com>
2012-12-17 22:10:08 -0500

    Updated example/code/readme

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 TeacupUI
 ========
 
-How to coming soon, until then checkout the example file.. The UI plugin is a work in progress.
+jQuery based simple representation of percentage data as a layer of ingredients in a tea cup. Yes, a teacup!! Read more at http://kufli.blogspot.com/2012/12/teacup-ui-fun-simple-to-use-jquery.html
+
+
+How to:
+
+	 <div id="Latte">
+		<a value="20" href="http://kufli.blogspot.com">Milk Foam</a>
+		<a value="50" href="http://github.com/karthik20522">Steamed Milk</a>
+		<a value="30" href="https://github.com/karthik20522/TeacupUI">Expresso</a>
+	</div>
+	
+	 $("#Latte").teacup();
+	 
+
+Custom colors:
+
+	<div id="White">
+		<a value="50" href="http://kufli.blogspot.com">Steamed Milk</a>
+		<a value="30" href="http://github.com/karthik20522">Expresso</a>
+	</div>
+	
+	 $("#White").teacup({ colors: ["#B9BC8D", "#E2640F"] });
+	 
+
+<b>Note that the values must be a total of 100</b>
+
--- teacup.js
+++ teacup.js
@@ -1,6 +1,7 @@
 ﻿jQuery.fn.teacup = function(options) {
     options = options || {};
-    options.colors = ["#42749F", "#E2640F", "#B9BC8D", "#BCD7D0", "#7EC2B3", "#C6DCAB"];
+    if(!options.colors)
+        options.colors = ["#B9BC8D", "#BCD7D0", "#E2640F", "#7EC2B3", "#C6DCAB", "#42749F"];
 
     this.each(function () {
         var input = this;
@@ -14,30 +15,38 @@ jQuery.teacup = function (input, options) {
     var htmlTeaCup = '<div class="teacup"><div class="outer">';
     var aLinks = $(input).find('a');
     var colorIndex = 0;
+    var count = 0;
 
     if (aLinks.length > 0) {
+        aLinks.each(function () {
+            count += parseInt($(this).attr('value'));
+        });
+
+        if (count < 100) 
+            htmlTeaCup += '<div style="height:' + (100 - count) + '%; background-color:white;" class="inner hover"><span></span></div>';
+
         aLinks.each(function (index, element) {
             var height = $(this).attr('value');
-            htmlTeaCup += '<div style="height:' + height + '%; background-color:' + getBackgroundColor() + '" class="inner hover' +
-                (index == aLinks.length - 1 ? " lastItem" : "") + '"><span><a href="' + $(this).attr("href") + '">' + $(this).text() + '</a></span></div>';
+
+            htmlTeaCup += '<div style="height:' + height + '%; background-color:' +
+                getBackgroundColor() + '" class="inner hover' +
+                (index == aLinks.length - 1 ? " lastItem" : "") + '"><span><a href="' +
+                $(this).attr("href") + '">' +
+                $(this).text() + '</a></span></div>';
         });
     }
+
     htmlTeaCup += '</div><div class="bottom"></div><div class="doughnut"></div>';
 
     $(input).html(htmlTeaCup);
 
     function getBackgroundColor() {
-        var color = '';
 
-        if (colorIndex == options.colors.length - 1) {
-            color = options.colors[colorIndex];
-            colorIndex = 0;
-        }
-        else {
-            color = options.colors[colorIndex];
+        if (colorIndex > options.colors.length)
+            colorIndex = 1;
+        else 
             colorIndex += 1;
-        }
 
-        return color;
+        return options.colors[colorIndex - 1];
     }
 }
